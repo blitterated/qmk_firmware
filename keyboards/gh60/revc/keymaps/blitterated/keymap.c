0a547a4621f4c15975600bbb2a1afa876f0109c9
blitterated <blitterated@protonmail.com>
2022-07-18 20:48:21 -0500

    customized gh60 keymap

--- keyboards/gh60/revc/keymaps/blitterated/keymap.c
+++ keyboards/gh60/revc/keymaps/blitterated/keymap.c
@@ -2,28 +2,31 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+    // Base Layer (Mac modifiers layout by default)
     [0] = LAYOUT_all( /* 0: qwerty */
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,
-        KC_LSFT, TG(2),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, KC_APP,  KC_RCTL
+        KC_GRAVE,         KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
+        KC_TAB,           KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        LCTL_T(KC_ESC),   KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,
+        KC_LSPO,          KC_NO,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_NO,
+        KC_LCTL,          KC_LALT,   KC_LGUI,                   KC_SPC,                                      KC_RGUI, TT(1),   KC_APP,  KC_RCTL
     ),
 
-    [1] = LAYOUT_all( /* 1: fn */
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
-        _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______,                                     _______, _______, _______, _______
+    // Poker II Layer 1 layout, Vim movement, Capslock, Layer 1 toggle
+    [1] = LAYOUT_all(
+        _______,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,       KC_F8,     KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL, KC_DEL,
+        _______,         _______, KC_UP,   _______, _______, _______, _______, _______,     KC_INS,    _______,  KC_PSCR, KC_SLCK, KC_PAUS, _______,
+        LCTL_T(KC_CAPS), KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN,     KC_UP,     KC_RGHT,  KC_HOME, KC_PGUP, KC_PENT, KC_PENT,
+        _______,         _______, _______, _______, _______, _______, _______, KC__VOLDOWN, KC__VOLUP, KC__MUTE, KC_END,  KC_PGDN, _______, _______,
+        TG(2),           _______, _______,                   _______,                                     _______, _______, _______, _______
     ),
 
-    [2] = LAYOUT_all( /* 2: arrows */
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______,
-        _______, _______, _______,                   _______,                                     _______, KC_LEFT, KC_DOWN, KC_RGHT
+    // Switch to Windows modifiers layout
+    [2] = LAYOUT_all(
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          KC_LGUI, KC_LALT,                   _______,                                     KC_RALT, _______, _______, _______
     ),
 
 };
